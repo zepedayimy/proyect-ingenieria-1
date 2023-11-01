bc2327cbe99a804e40ce669f0f58907b79f9912d
Jimmy Zepeda <yfs8jzdjxtc6csq8ip0a21xifmkokm@bots.bitbucket.org>
2023-11-01 03:02:38 +0100

    rrr

---
+++ src/test/java/Proyectosoftware/ProyectIngenieraSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package Proyectosoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectIngenieraSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
